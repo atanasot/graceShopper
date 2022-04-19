ff7fa0c16662a9394fc35a886096fceda1215b42
Joshua Reyes <reyes.j15@gmail.com>
2022-04-19 15:42:29 -0400

    BeerDescription component added

---
+++ client/components/BeerDescription.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import store from "../store";
+import { connect } from "react-redux";
+
+class BeerDescription extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return <p>Beer Description will be inserted here</p>;
+  }
+}
+
+export default connect(null)(BeerDescription);
