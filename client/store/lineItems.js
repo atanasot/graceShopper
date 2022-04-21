634f7db905f72c372c6d0d8f20d237408f317d7c
Lina Ryerkerk <lina.ryerkerk@gmail.com>
2022-04-21 11:45:16 -0400

    can add beer to lineItems

--- client/store/lineItems.js
+++ client/store/lineItems.js
@@ -1,21 +1,21 @@
-import axios from "axios"
+import axios from "axios";
 
 // make a backend route /api/lineItems
 
 /**
  * ACTION TYPES
  */
- const ADD_QTY = "ADD_QTY";
+const ADD_QTY = "ADD_QTY";
 
- const DELETE_BEER = "DELETE_BEER"
+const DELETE_BEER = "DELETE_BEER";
 
- /**
+/**
  * ACTION CREATORS
  */
 
-const _addBeer = (beer) => ({type: ADD_QTY, beer})
+const _addBeer = (beer) => ({ type: ADD_QTY, beer });
 
-const _deleteBeer = (beer) => ({type: DELETE_BEER, beer})
+const _deleteBeer = (beer) => ({ type: DELETE_BEER, beer });
 
 /**
  * THUNK CREATORS
@@ -23,24 +23,20 @@ const _deleteBeer = (beer) => ({type: DELETE_BEER, beer})
 
 // beer here represented as {id : quantity}
 export const addBeer = (beer) => {
-    return async(dispatch) => {
-        const response = await axios.post('/api/lineItems', beer)
-        dispatch(_addBeer(response.data))
-    }
+  return async (dispatch) => {
+    const response = await axios.post("/api/lineItems", beer);
+    dispatch(_addBeer(response.data));
+  };
+};
+
+export default function (state = [], action) {
+  switch (action.type) {
+    case ADD_QTY:
+      return [...state, action.beer];
+    default:
+      return state;
+  }
 }
-
-export default function lineItems (state = {}, action) {
-    if (action.type === ADD_QTY) {
-        return {...state, ...action.beer}
-        
-    }
-    if (action.type === DELETE_BEER) {
-        return
-    }
-    return state
-}
-
-
 
 // need to add more cases
 // add wines as well
--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, '..', 'public')))
 app.use('/api/wines', require('./api/routes/wines'));
 app.use('/api/beers', require('./api/routes/beers'));
 app.use('/api/orders', require('./api/routes/orders'));
-
+app.use('/api/lineItems', require('./api/routes/lineItems'));
 
 // any remaining requests with an extension (.js, .css, etc.) send 404
 app.use((req, res, next) => {
