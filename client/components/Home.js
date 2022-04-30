e3dd0c5fb1bb0b353ca2a7e2a51e1aad40d6050d
Kenneth Lee <95536900+kennethlee0502@users.noreply.github.com>
2022-04-30 16:30:01 -0400

    hello

--- client/components/Home.js
+++ client/components/Home.js
@@ -6,11 +6,19 @@ const Home = () => {
   return (
     <div>
       <div className="homePage">
+        {" "}
         <div className="homePageword">
           <p>Every sip has a story</p>
         </div>
       </div>
-      <div className="margin"></div>
+      <div
+        style={{ height: "1000px", backgroundColor: "white", fontSize: "36px" }}
+      >
+        {" "}
+        Scroll Up and Down this page to see the parallax scrolling effect. This
+        div is just here to enable scrolling. Tip: Try to remove the
+        background-attachment property to remove the scrolling effect.
+      </div>
     </div>
   );
 };
