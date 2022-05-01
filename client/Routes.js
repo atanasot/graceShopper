4f2a467e8f4bde3dfae5291140050fa0dca3b76e
Jennifer Woodbury <jenann125@yahoo.com>
2022-04-30 21:50:33 -0400

    Update to admin page and beer description before login update

--- client/Routes.js
+++ client/Routes.js
@@ -37,7 +37,7 @@ class Routes extends Component {
   }
 
   render() {
-    const { isLoggedIn } = this.props;
+    const { isLoggedIn, isAdmin } = this.props;
     return (
       <div>
         {isLoggedIn ? (
@@ -82,6 +82,7 @@ const mapState = (state) => {
     // Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
     // Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
     isLoggedIn: !!state.auth.id,
+    isAdmin: state.auth.isAdmin,
   };
 };
 
