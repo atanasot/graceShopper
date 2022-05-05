16079147f866c945edf02c3dbe4691e68e7e86de
Lina Ryerkerk <lina.ryerkerk@gmail.com>
2022-05-05 16:11:20 -0400

    added address model, displayed details in profile

--- client/components/AboutWine.js
+++ client/components/AboutWine.js
@@ -7,11 +7,11 @@ const AboutWine = () => {
   return (
     <div id="parallax-world-of-ugg">
       <section>
-        <div class="title">
+        <div className="title">
           <h1> Wines </h1>
         </div>
       </section>
-      <section class="block">
+      <section className="block">
         <div>
           <h2
             style={{
@@ -87,7 +87,7 @@ const AboutWine = () => {
         </div>
       </section>
 
-      <section class="block">
+      <section className="block">
         <h2
           style={{
             color: "black",
--- server/db/index.js
+++ server/db/index.js
@@ -7,6 +7,7 @@ const Wine = require("./models/Wine");
 const Beer = require("./models/Beer");
 const Order = require("./models/Order");
 const LineItem = require("./models/LineItem");
+const Address = require("./models/Address");
 
 //associations could go here!
 Order.belongsTo(User);
@@ -17,7 +18,9 @@ LineItem.belongsTo(Wine);
 Wine.hasMany(LineItem);
 LineItem.belongsTo(Beer);
 Beer.hasMany(LineItem);
-LineItem.belongsTo(User);
+//LineItem.belongsTo(User);
+Address.belongsTo(User);
+User.hasOne(Address)
 
 module.exports = {
   db,
@@ -27,5 +30,6 @@ module.exports = {
     Beer,
     Order,
     LineItem,
+    Address,
   },
 };
