59b6f629eac493a58f7815a702bb978b59c81cb4
Joshua Reyes <reyes.j15@gmail.com>
2022-05-06 14:35:34 -0400

    added sort functionality

--- client/components/Beers.js
+++ client/components/Beers.js
@@ -2,10 +2,11 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
-const Beers = ({ beers }) => {
+const Beers = ({ beers, match,history  }) => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+
   return (
     <div>
       <div style={{ marginLeft: "270px", marginTop: "100px" }}>
@@ -15,6 +16,15 @@ const Beers = ({ beers }) => {
         <div className="container">
           <div style={{ marginBottom: "70px" }}>
             <h1>BEER</h1>
+            <div>
+              <select value={ match.params.sort } onChange={ ev =>  history.push(`/beer?sort=${ev.target.value}`)}>
+                <option value=''> Sort By</option>
+                <option value='name,asc'> Name ASC</option>
+                <option value='name,desc'> Name DESC</option>
+                <option value='price,asc'> Price ASC</option>
+                <option value='price,desc'> Price DESC</option>
+              </select>
+            </div>
           </div>
           <ol className="posts">
             {beers.map((beer) => (
@@ -32,7 +42,7 @@ const Beers = ({ beers }) => {
                         <p className="date1">{beer.type} </p>
                         <div className="pricing-and-cart1">
                           <div className="pricing1">
-                            <p className="current-price1">${beer.price}</p>
+                            <p className="current-price1">${parseFloat(beer.price).toFixed(2)}</p>
                           </div>
                           <i className="fas fa-shopping-cart1" />
                         </div>
@@ -49,8 +59,19 @@ const Beers = ({ beers }) => {
   );
 };
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state, { location, match }) => {
   const { beers } = state;
+
+  console.log(match.params)
+  
+  const searchParams = new URLSearchParams(location.search) // ?sort=name,asc
+  const sort = searchParams.get('sort');
+
+  if (sort === 'name,asc') {
+    console.log('sort by name asc');
+    beers.sort((a,b) => a.name.localeCompare(b.name))
+  }
+
   return {
     beers,
   };
