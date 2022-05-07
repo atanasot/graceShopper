3452d9969929f2cd4f8e27dd42a8ba20a76e5dc8
Joshua Reyes <reyes.j15@gmail.com>
2022-05-07 11:18:41 -0400

    added sort function for wines

--- client/components/Wines.js
+++ client/components/Wines.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
-const Wines = ({ wines }) => {
+const Wines = ({ wines, match, history }) => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -29,6 +29,15 @@ const Wines = ({ wines }) => {
         <div className="container">
           <div style={{ marginBottom: "70px" }}>
             <h1>WINE</h1>
+            <div>
+              <select value={ match.params.sort } onChange={ ev =>  history.push(`/wine?sort=${ev.target.value}`)}>
+                <option value=''> Sort By</option>
+                <option value='name,asc'> Name ASC</option>
+                <option value='name,desc'> Name DESC</option>
+                <option value='price,asc'> Price ASC</option>
+                <option value='price,desc'> Price DESC</option>
+              </select>
+            </div>
           </div>
           <ol className="posts">
             {wines.map((wine) => (
@@ -74,8 +83,24 @@ const Wines = ({ wines }) => {
   );
 };
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state, {location, match}) => {
   const { wines } = state;
+  const searchParams = new URLSearchParams(location.search) // ?sort=name,asc
+  const sort = searchParams.get('sort');
+
+  if (sort === 'name,asc') {
+    console.log('sort by name asc');
+    wines.sort((a,b) => a.name.localeCompare(b.name))
+  }
+  else if (sort === 'name,desc') {
+    wines.sort((a,b) => b.name.localeCompare(a.name))
+  }
+  else if (sort === 'price,asc') {
+    wines.sort((a,b) => parseFloat(a.price) - parseFloat(b.price))
+  }
+  else if (sort === 'price,desc') {
+    wines.sort((a,b) => parseFloat(b.price) - parseFloat(a.price))
+  }
   return {
     wines,
   };
